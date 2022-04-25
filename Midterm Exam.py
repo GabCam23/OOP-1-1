a2a23bed3712781410ec4852b16888f773aa127a
GabCam23 <92899666+GabCam23@users.noreply.github.com>
2022-04-25 15:41:54 +0800

    Add files via upload

---
+++ Midterm Exam.py
@@ -0,0 +1,36 @@
+from tkinter import *
+window = Tk()
+
+window.geometry("600x500+30+20")
+window.title("Midterms in OOP")
+
+def your_name():
+    fullname = fullname_yn.get()
+
+    txtfld.insert(0,f'{fullname}')
+
+fullname_yn = Entry(window, bd = 3,fg="maroon",bg="lightgreen")
+fullname_yn.place(x=300,y=100)
+
+lbl = Label(window, text = "Enter your Fullname",  fg="maroon",bg="lightgreen")
+lbl.place(x=60, y=100)
+
+btn = Button(window,text = "Click to display your Fullname",fg="maroon",bg="lightgreen", command=your_name)
+btn.place(x=60,y=150)
+btn.pack
+
+txtfld = Entry(window, bd = 3,fg="maroon",bg="lightgreen")
+txtfld.place(x=300,y=150)
+txtfld.pack
+
+fullname_yn.pack
+
+window.mainloop()
+
+
+
+
+
+
+
+
